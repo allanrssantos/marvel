579ba98214977f276f2d14deda3430ccbc7deea8
Allan Santos <allansssoares@gmail.com>
2024-01-21 18:53:41 -0300

    dev: criando o tabs para apresentação.

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { useLocation, Link } from "react-router-dom";
+
+interface TabProps {
+  isActive: boolean;
+}
+
+const StyledLi = styled.li<TabProps>`
+  cursor: pointer;
+  margin-right: 10px;
+  padding: 8px;
+  font-weight: ${(props) => (props.isActive ? "bold" : "normal")};
+  color: ${(props) => (props.isActive ? "#007bff" : "#e60000")};
+  border-bottom: 2px solid ${(props) => (props.isActive ? "#007bff" : "transparent")};
+  transition: color 0.3s, border-bottom 0.3s;
+
+  a {
+    color: ${(props) => (props.isActive ? "#007bff" : "#e60000")};
+    text-decoration: none;
+  }
+`;
+
+
+interface TabsProps {
+  tabs: string[];
+  onChangeTab: (tab: string) => void;
+}
+
+const TabsContainer = styled.div`
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    overflow-x: auto;
+    white-space: nowrap;
+    border-bottom: 1px solid #ddd;
+  }
+`;
+
+const Tabs: React.FC<TabsProps> = ({ tabs, onChangeTab }) => {
+  const [activeTab, setActiveTab] = useState(tabs[0]);
+  const location = useLocation();
+
+  const handleTabChange = (tab: string) => {
+    setActiveTab(tab);
+    onChangeTab(tab);
+  };
+
+  const isTabsRoute = location.pathname !== "/";
+
+  return isTabsRoute ? (
+    <TabsContainer>
+      <ul>
+        {tabs.map((tab) => (
+          <StyledLi
+            key={tab}
+            onClick={() => handleTabChange(tab)}
+            isActive={tab === activeTab}
+          >
+           <Link to={`/${tab.toLowerCase()}`}>{tab}</Link>
+          </StyledLi>
+        ))}
+      </ul>
+    </TabsContainer>
+  ) : null;
+};
+
+export default Tabs;
