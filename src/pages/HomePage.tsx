a2d5b466178ba75f20298da76329645500148e70
Allan Santos <allansssoares@gmail.com>
2024-01-21 18:50:32 -0300

    dev: criando a home page.

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyledHomePage } from "../styles/HomePageStyles";
+import KeyForm from "../components/KeyForm";
+
+const HomePage: React.FC = () => {
+  return (    
+      <StyledHomePage>
+        <KeyForm />
+      </StyledHomePage>    
+  );
+};
+
+export default HomePage;
---
+++ src/styles/HomePageStyles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import img from "../styles/img/marvel.jpg"
+
+export const StyledHomePage = styled.div`
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  top: 0;
+  left: 0;
+  background: url(${img});
+`;
